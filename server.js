4bf1463bd409157187f2b66bb4788dc13aea2709
mohamedfathycreation <m.fathy@creation.sa>
2025-04-12 19:19:42 +0200

    update server file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
+const auth = require('json-server-auth');
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.db = router.db;
+
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 server.listen(process.env.PORT || 3600, "0.0.0.0", () => {
   console.log('JSON Server is running');
